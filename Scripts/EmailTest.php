bfd590c504b980b3e4082517cdf74a1af5ac2b77
Oliver Klee <typo3-coding@oliverklee.de>
2014-09-02 14:15:43 +0200

    [FEATURE] Retrieve and validate the user.

--- Scripts/EmailTest.php
+++ Scripts/EmailTest.php
@@ -22,5 +22,31 @@ if (!defined('TYPO3_cliMode')) {
 	die('You cannot run this script directly!');
 }
 
-echo 'Test' . LF;
+echo 'Retrieving CLI back-end user.' .  LF;
+/** @var tx_oelib_Mapper_BackEndUser $userMapper */
+$userMapper = Tx_Oelib_MapperRegistry::get('tx_oelib_Mapper_BackEndUser');
+$user = $userMapper->findByCliKey();
+if ($user === NULL) {
+	echo 'Not found. Stopping.' . LF;
+	exit;
+}
+echo 'User found. UID: ' . $user->getUid() . LF;
+
+$emailAddress = $user->getEmailAddress();
+if ($emailAddress === '') {
+	echo 'The user does not have an e-mail address. Stopping.' . LF;
+	exit;
+}
+if ($user->getName() === '') {
+	echo 'The user does not have a name address. Stopping.' . LF;
+	exit;
+}
+if (!preg_match('/[\-_\.a-zA-Z0-9]+@localhost/', $emailAddress) && !t3lib_div::validEmail($emailAddress)) {
+	echo 'The e-mail address "' . $emailAddress . '" is not valid. Stopping.' . LF;
+	exit;
+}
+
+echo 'The user "' . $user->getName() . '" <' . $user->getEmailAddress() . '> seems to be okay.' . LF;
+
+echo LF . 'Finished!' . LF;
 
