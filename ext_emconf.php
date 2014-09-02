5f485607f29cd0757a7bb3a304088a537026f3f0
Oliver Klee <typo3-coding@oliverklee.de>
2014-09-02 14:56:17 +0200

    [TASK] Release version 1.0.0.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "emailtest".
  *
- * Auto generated 02-09-2014 13:52
+ * Auto generated 02-09-2014 14:56
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,7 +11,7 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'E-mail test',
+	'title' => 'E-mail tester',
 	'description' => 'This extension provides a command-line tool for testing the various ways of sending e-mail.',
 	'category' => 'test',
 	'author' => 'Oliver Klee',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-5.5.99',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:4:{s:9:"CHANGELOG";s:4:"3531";s:12:"ext_icon.gif";s:4:"3186";s:7:"LICENSE";s:4:"e8c1";s:9:"README.md";s:4:"33ce";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"CHANGELOG";s:4:"3531";s:12:"ext_icon.gif";s:4:"b4bf";s:17:"ext_localconf.php";s:4:"318d";s:7:"LICENSE";s:4:"e8c1";s:9:"README.md";s:4:"ae51";s:21:"Scripts/EmailTest.php";s:4:"3232";}',
 	'suggests' => array(
 	),
 );
